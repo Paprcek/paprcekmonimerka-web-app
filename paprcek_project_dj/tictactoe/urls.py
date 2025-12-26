e0b86407bebce777cc6856440974472e6d9f0b45
Paprcek <moncakbp@gmail.com>
2025-12-26 23:13:03 +0100

    Přidána hra TicTacToe proti AI

--- paprcek_project_dj/tictactoe/urls.py
+++ paprcek_project_dj/tictactoe/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.tictactoe_game, name='tictactoe_game'),
+    path('', views.tictactoe_game, name='tictactoe'),
+    path('move/', views.ai_move, name='ai_move'),
 ]
