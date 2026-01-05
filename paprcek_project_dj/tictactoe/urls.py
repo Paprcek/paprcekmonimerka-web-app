c8585cc6c2792ff2c3751a53a0628493e507be66
Paprcek <moncakbp@gmail.com>
2026-01-05 18:20:55 +0100

    Přidání světového rekordu k tictactoe

--- paprcek_project_dj/tictactoe/urls.py
+++ paprcek_project_dj/tictactoe/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.tictactoe_game, name='tictactoe'),
     path('move/', views.ai_move, name='ai_move'),
+    path('update-record/', views.update_tictactoe_record, name='update_tictactoe_record'),
 ]
